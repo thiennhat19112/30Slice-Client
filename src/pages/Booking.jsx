ce1b28a6a4246027eabaf3f729656c251ae01ba0
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-10-21 13:06:12 +0700

    Update Booking.jsx

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -2,9 +2,11 @@ import { useState, useRef, useEffect } from 'react';
 
 
 function Booking(props) {
+
     const [activeId, setActiveId] = useState('');
     const [EmployeeId, setEmployeeId] = useState(0);
     const [listTime, setListTime] = useState();
+    const [arrEmployee, setArrEmployee] = useState([]);
     const getCurrentDate = () => {
         return new Date().toLocaleDateString('en', {
             year: "2-digit",
@@ -13,50 +15,9 @@ function Booking(props) {
         })
     }
     const refDate = useRef(getCurrentDate());
-    const refEmployee = useRef(0);
-    useEffect(() => {
-        reloadListTime();
-    }, [activeId, EmployeeId]);
-    class Booking {
-        constructor(customer, employee) {
-            this.customer = customer
-            this.employee = employee
-        }
-    }
+
     const allAvailableTime = ['07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00']
     const arrBookedData = {}
-    const arrEmployee = [
-        {
-            id: 1,
-            name: 'Nguyễn Văn A',
-            shifts: ['07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00'],
-            status: 'active',
-            statusCode: 1
-        },
-        {
-            id: 2,
-            name: 'Nguyễn Văn B',
-            shifts: ['07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00'],
-            status: 'active',
-            statusCode: 1
-        },
-        {
-            id: 3,
-            name: 'Nguyễn Văn C',
-            shifts: ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00'],
-            status: 'active',
-            statusCode: 1
-        },
-        {
-            id: 4,
-            name: 'Nguyễn Văn D',
-            shifts: ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00'],
-            status: 'disabled',
-            statusCode: 0
-        },
-
-    ]
-
     const arrDate = []
     for (var i = 0; i <= 6; i++) {
         var date = new Date();
@@ -77,16 +38,31 @@ function Booking(props) {
 
         )
     }
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_ENDPOINT + 'stylelist/gettAllStyleList')
+            .then(res => res.json())
+            .then(data => {
+                setArrEmployee(data);
+            })
+    }, []);
+    useEffect(() => {
+        reloadListTime()
+    }, [activeId, EmployeeId])
 
-    const getAvailableTime = (bookedDate, employeeId) => {
+
+
+
+    function getAvailableTime(bookedDate, employeeId) {
         // if (!arrBookedData[bookedDate]) {
         //     arrBookedData[bookedDate] = new Array(allAvailableTime.length).fill(new Array())
         // }
+        console.log(employeeId);
         let arrAvailableTime = []
         if (employeeId == 0) {
-            arrAvailableTime = [...new Set(arrEmployee.flatMap(num => num.shifts))]
+            arrAvailableTime = [...new Set(arrEmployee.flatMap(num => num.Info.Shifts))]
+            console.log(arrEmployee)
         } else {
-            arrAvailableTime = arrEmployee.find(ele => ele.id == employeeId).shifts
+            arrAvailableTime = arrEmployee.find(ele => ele._id == employeeId).Info.Shifts
         }
         return arrAvailableTime.filter((time, index) => {
             // return arrBookedTime[index].length <= 1 && new Date(`${bookedDate} ${time}`).getTime() > new Date().getTime()
@@ -147,7 +123,7 @@ function Booking(props) {
     const bookingSave = () => {
         console.log(activeId)
         console.log(refDate.current.value)
-        console.log(refEmployee.current.value)
+        console.log(EmployeeId)
     }
 
 
@@ -183,11 +159,11 @@ function Booking(props) {
                             <label className="btn btn-outline-warning rounded-5" htmlFor='randomEmployee'>Ngẫu nhiên</label>
                         </div>
                         {arrEmployee.map((item, index) => {
-                            if (item.status == 'active' && item.statusCode == 1) {
+                            if (item.Info.Status == 'active' && item.Info.Status_Code == 0) {
                                 return (
                                     <div key={index} className='col'>
-                                        <input type="radio" className="btn-check" name="options-outlined" id={item.id} onChange={() => setEmployeeId(item.id)} onClick={() => reloadListTime()} />
-                                        <label className="btn btn-outline-warning  rounded-5" htmlFor={item.id}>{item.name}</label>
+                                        <input type="radio" className="btn-check" name="options-outlined" id={item._id} onChange={() => setEmployeeId(item._id)} onClick={() => reloadListTime()} />
+                                        <label className="btn btn-outline-warning  rounded-5" htmlFor={item._id}>{item.Full_Name}</label>
                                     </div>
                                 )
                             }
