f3e638028d31475b42254e36c0752a537b098019
Trình Thanh Phong <dinhmenhhathui@gmail.com>
2022-11-17 17:52:33 +0700

    html to jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,143 +1,142 @@
 const Login = () => {
   return (
     <div>
-      <main class="main-content">
-        <div class="signUP-admin">
-          <div class="container-fluid">
-            <div class="row justify-content-center">
-              <div class="col-xl-4 col-lg-5 col-md-5 p-0">
-                <div class="signUP-admin-left signIn-admin-left position-relative">
-                  <div class="signUP-overlay">
+      <main className="main-content">
+        <div className="signUP-admin">
+          <div className="container-fluid">
+            <div className="row justify-content-center">
+              <div className="col-xl-4 col-lg-5 col-md-5 p-0">
+                <div className="signUP-admin-left signIn-admin-left position-relative">
+                  <div className="signUP-overlay">
                     <img
-                      class="svg signupTop"
+                      className="svg signupTop"
                       src="img/svg/signuptop.svg"
                       alt="img"
                     />
                     <img
-                      class="svg signupBottom"
+                      className="svg signupBottom"
                       src="img/svg/signupbottom.svg"
                       alt="img"
                     />
                   </div>
-                  <div class="signUP-admin-left__content">
-                    <div class="text-capitalize mb-md-30 mb-15 d-flex align-items-center justify-content-md-start justify-content-center">
+                  <div className="signUP-admin-left__content">
+                    <div className="text-capitalize mb-md-30 mb-15 d-flex align-items-center justify-content-md-start justify-content-center">
                       <a
-                        class="wh-36 bg-primary text-white radius-md mr-10 content-center"
+                        className="wh-36 bg-primary text-white radius-md mr-10 content-center"
                         href="index.html"
                       >
                         a
                       </a>
-                      <span class="text-dark">admin</span>
+                      <span className="text-dark">admin</span>
                     </div>
                     <h1>Bootstrap 4 React Web Application</h1>
                   </div>
-                  <div class="signUP-admin-left__img">
+                  <div className="signUP-admin-left__img">
                     <img
-                      class="img-fluid svg"
+                      className="img-fluid svg"
                       src="img/svg/signupIllustration.svg"
                       alt="img"
                     />
                   </div>
                 </div>
               </div>
-              <div class="col-xl-8 col-lg-7 col-md-7 col-sm-8">
-                <div class="signUp-admin-right signIn-admin-right  p-md-40 p-10">
-                  <div class="signUp-topbar d-flex align-items-center justify-content-md-end justify-content-center mt-md-0 mb-md-0 mt-20 mb-1">
-                    <p class="mb-0">
+              <div className="col-xl-8 col-lg-7 col-md-7 col-sm-8">
+                <div className="signUp-admin-right signIn-admin-right  p-md-40 p-10">
+                  <div className="signUp-topbar d-flex align-items-center justify-content-md-end justify-content-center mt-md-0 mb-md-0 mt-20 mb-1">
+                    <p className="mb-0">
                       Don't have an account?
-                      <a href="sign-up.html" class="color-primary">
+                      <a href="sign-up.html" className="color-primary">
                         Sign up
                       </a>
                     </p>
                   </div>
-                  <div class="row justify-content-center">
-                    <div class="col-xl-7 col-lg-8 col-md-12">
-                      <div class="edit-profile mt-md-25 mt-0">
-                        <div class="card border-0">
-                          <div class="card-header border-0  pb-md-15 pb-10 pt-md-20 pt-10 ">
-                            <div class="edit-profile__title">
+                  <div className="row justify-content-center">
+                    <div className="col-xl-7 col-lg-8 col-md-12">
+                      <div className="edit-profile mt-md-25 mt-0">
+                        <div className="card border-0">
+                          <div className="card-header border-0  pb-md-15 pb-10 pt-md-20 pt-10 ">
+                            <div className="edit-profile__title">
                               <h6>
-                                Sign up to{" "}
-                                <span class="color-primary">Admin</span>
+                                Sign up to{"{"}" "{"}"}
+                                <span className="color-primary">Admin</span>
                               </h6>
                             </div>
                           </div>
-                          <div class="card-body">
-                            <div class="edit-profile__body">
-                              <div class="form-group mb-20">
-                                <label for="username">
+                          <div className="card-body">
+                            <div className="edit-profile__body">
+                              <div className="form-group mb-20">
+                                <label htmlFor="username">
                                   Username or Email Address
                                 </label>
                                 <input
                                   type="text"
-                                  class="form-control"
+                                  className="form-control"
                                   id="username"
                                   placeholder="Username"
                                 />
                               </div>
-                              <div class="form-group mb-15">
-                                <label for="password-field">password</label>
-                                <div class="position-relative">
+                              <div className="form-group mb-15">
+                                <label htmlFor="password-field">password</label>
+                                <div className="position-relative">
                                   <input
                                     id="password-field"
                                     type="password"
-                                    class="form-control"
+                                    className="form-control"
                                     name="password"
-                                    value="secret"
+                                    defaultValue="secret"
                                   />
-                                  <div class="fa fa-fw fa-eye-slash text-light fs-16 field-icon toggle-password2"></div>
+                                  <div className="fa fa-fw fa-eye-slash text-light fs-16 field-icon toggle-password2" />
                                 </div>
                               </div>
-                              <div class="signUp-condition signIn-condition">
-                                <div class="checkbox-theme-default custom-checkbox ">
+                              <div className="signUp-condition signIn-condition">
+                                <div className="checkbox-theme-default custom-checkbox ">
                                   <input
-                                    class="checkbox"
+                                    className="checkbox"
                                     type="checkbox"
                                     id="check-1"
                                   />
-                                  <label for="check-1">
-                                    <span class="checkbox-text">
+                                  <label htmlFor="check-1">
+                                    <span className="checkbox-text">
                                       Keep me logged in
                                     </span>
                                   </label>
                                 </div>
-                                <a href="forget-password.html">
-                                  forget password
-                                </a>
+                                <a href="forget-password.html">forget password</a>
                               </div>
-                              <div class="button-group d-flex pt-1 justify-content-md-start justify-content-center">
-                                <button class="btn btn-primary btn-default btn-squared mr-15 text-capitalize lh-normal px-50 py-15 signIn-createBtn ">
+                              <div className="button-group d-flex pt-1 justify-content-md-start justify-content-center">
+                                <button className="btn btn-primary btn-default btn-squared mr-15 text-capitalize lh-normal px-50 py-15 signIn-createBtn ">
                                   sign in
                                 </button>
                               </div>
-                              <p class="social-connector text-center mb-sm-25 mb-15  mt-sm-30 mt-20">
+                              <p className="social-connector text-center mb-sm-25 mb-15  mt-sm-30 mt-20">
                                 <span>Or</span>
                               </p>
-                              <div class="button-group d-flex align-items-center justify-content-md-start justify-content-center">
-                                <ul class="signUp-socialBtn">
+                              <div className="button-group d-flex align-items-center justify-content-md-start justify-content-center">
+                                <ul className="signUp-socialBtn">
                                   <li>
-                                    <button class="btn text-dark px-30">
+                                    <button className="btn text-dark px-30">
                                       <img
-                                        class="svg"
+                                        className="svg"
                                         src="img/svg/google.svg"
                                         alt="img"
-                                      />{" "}
+                                      />
+                                      {"{"}" "{"}"}
                                       Sign up with Google
                                     </button>
                                   </li>
                                   <li>
-                                    <button class=" radius-md wh-48 content-center">
+                                    <button className=" radius-md wh-48 content-center">
                                       <img
-                                        class="svg"
+                                        className="svg"
                                         src="img/svg/facebook.svg"
                                         alt="img"
                                       />
                                     </button>
                                   </li>
                                   <li>
-                                    <button class="radius-md wh-48 content-center">
+                                    <button className="radius-md wh-48 content-center">
                                       <img
-                                        class="svg"
+                                        className="svg"
                                         src="img/svg/twitter.svg"
                                         alt="img"
                                       />
@@ -158,12 +157,12 @@ const Login = () => {
         </div>
       </main>
       <div id="overlayer">
-        <span class="loader-overlay">
-          <div class="atbd-spin-dots spin-lg">
-            <span class="spin-dot badge-dot dot-primary"></span>
-            <span class="spin-dot badge-dot dot-primary"></span>
-            <span class="spin-dot badge-dot dot-primary"></span>
-            <span class="spin-dot badge-dot dot-primary"></span>
+        <span className="loader-overlay">
+          <div className="atbd-spin-dots spin-lg">
+            <span className="spin-dot badge-dot dot-primary" />
+            <span className="spin-dot badge-dot dot-primary" />
+            <span className="spin-dot badge-dot dot-primary" />
+            <span className="spin-dot badge-dot dot-primary" />
           </div>
         </span>
       </div>
