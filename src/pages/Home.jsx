eeed82d8a161267efbbcd94e94ffdfd3342eb5f4
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-11-19 13:33:09 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,17 @@ function Home() {
   return (
     <>
       <div className="atbd-page-content">
-        <div className="container-fluid">
+        <div className="container-fluid m-0 p-0">
+          <img
+            src="https://4rau.vn/upload/hinhanh/dsc01875-8577.jpg"
+            alt=""
+            width="100%"
+            height="100%"
+          />
+       
+
           <div className="row">
-            <h1>Trang chủ nè mấy cha</h1>
+            <h1></h1>
           </div>
         </div>
       </div>
